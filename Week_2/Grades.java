dd1f3370a1264d88e2c4f5609b2072ebb41321a7
duygucgun <83750292+duygucgun@users.noreply.github.com>
2021-07-10 03:39:56 +0300

    Add files via upload

---
+++ Week_2/Grades.java
@@ -0,0 +1,47 @@
+package week2;
+import java.util.Scanner;
+
+public class LetterGrade{
+    public static void main(String [] args){
+        int vize_notu, final_notu;
+        double sonuc; 
+        Scanner scan= new Scanner (System.in);
+        System.out.println ("Vize Notunuzu Giriniz:");
+        vize_notu=scan.nextInt();
+
+        System.out.println ("Final Notunuzu Giriniz:");
+        final_notu=scan.nextInt();
+
+        sonuc=(vize_notu*0.40 + final_notu*0.60) ; //Vize notunun %40'ı Final notunun %60'ı alınır ve ortalama hesaplanır. //
+        System.out.println ("Ders Notunuz: " + sonuc);
+
+        if (sonuc>85){ //85 ve üstü AA//
+            System.out.println ("Harf Notunuz AA");
+        
+        }
+        else if (sonuc<85 && sonuc > 80) { // 85 ve 80 arası BA //
+            System.out.println ("Harf Notunuz BA");
+        }
+        else if (sonuc<80 && sonuc>75){ //75 ve 80 arası BB//
+            System.out.println("Harf Notunuz BB");
+        
+        }
+        else if (sonuc<75 && sonuc>65 ){ //75 ve 65 arası CB//
+            System.out.println ("Harf Notunuz CB");
+        }
+        else if (sonuc<65 && sonuc>60){ //65 ve 60 arası CC
+            System.out.println ("Harf Notunuz CC");
+        }
+        else if (sonuc<60 && sonuc>55){ //55 ve 60 arası CD //
+            System.out.println ("Harf Notunuz CD");
+
+        }
+        else if (sonuc<55 && sonuc>50){ // 55 ve 50 arası DD //
+            System.out.println("Harf Notunuz DD");
+        
+        }
+        else { //50'den düşük ortalama ise FF olarak görünüyor. //
+            System.out.println("Harf Notunuz FF Başarısız Oldunuz!");
+        }
+    }
+}
---
+++ Week_2/Stationery.java
@@ -0,0 +1,8 @@
+package week2;
+import java.util.Scanner;
+public class Stationery{
+    public static void main(String[] args) {
+      
+         HashMap<String
+    }
+}
